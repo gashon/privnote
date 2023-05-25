9c699a088ef264a5458829078ad7042f249ef9c4
GashonHussein <gashon96@gmail.com>
2023-05-24 22:43:56 -0700

    feat: add migrations

---
+++ src/db/migrations/logs/create-secret-table.ts
@@ -0,0 +1,39 @@
+import { Kysely, sql } from 'kysely';
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable('secret')
+    .addColumn('id', 'serial', (col) => col.primaryKey().autoIncrement())
+    .addColumn('owner_id', 'integer', (col) =>
+      col.notNull().references('user.id'),
+    )
+    .addColumn('key', 'varchar', (col) => col.unique().notNull())
+    .addColumn('token', 'varchar', (col) => col.notNull())
+    .addColumn('views', 'integer', (col) => col.notNull().defaultTo(0))
+    .addColumn('max_views', 'integer', (col) => col.notNull().defaultTo(1))
+    .addColumn('expires_at', 'timestamp')
+    .addColumn('deleted_at', 'timestamp')
+    .addColumn('created_at', 'timestamp', (col) =>
+      col.defaultTo(sql`now()`).notNull(),
+    )
+    .addColumn('updated_at', 'timestamp', (col) =>
+      col.defaultTo(sql`now()`).notNull(),
+    )
+    .execute();
+
+  await db.schema
+    .createIndex('secret_owner_id_index')
+    .on('secret')
+    .column('owner_id')
+    .execute();
+
+  await db.schema
+    .createIndex('secret_key_index')
+    .on('secret')
+    .column('key')
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('secret').execute();
+}
