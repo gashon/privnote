2c8506d672921693e3c8af1402b1b7fe1778b1d6
GashonHussein <gashon96@gmail.com>
2023-05-25 15:40:42 -0700

    fix: env var naming

--- src/db/lib/kysely.ts
+++ src/db/lib/kysely.ts
@@ -4,17 +4,12 @@ import { Pool } from 'pg';
 import { Kysely, PostgresDialect } from 'kysely';
 import { Database } from '@/db/models';
 
-console.log(
-  'connecting to',
-  process.env.POSTGRES_DB,
-  process.env.POSTGRES_PORT,
-);
 export const db = new Kysely<Database>({
   dialect: new PostgresDialect({
     pool: new Pool({
-      host: process.env.POSTGRES_DB,
+      host: process.env.POSTGRES_HOST,
       port: Number(process.env.POSTGRES_PORT),
-      database: process.env.POSTGRES_NAME,
+      database: process.env.POSTGRES_DB,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
     }),
