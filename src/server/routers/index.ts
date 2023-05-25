f738c16f6a97010258d9762d12d532222ce0e65f
GashonHussein <gashon96@gmail.com>
2023-05-25 16:23:24 -0700

    feat: create secretTable router

--- src/server/routers/index.ts
+++ src/server/routers/index.ts
@@ -1,7 +1,9 @@
 import { publicProcedure, router } from '@/server/lib/trpc';
+import { secretRouter } from '@/server/routers/secret';
 
 export const appRouter = router({
   healthcheck: publicProcedure.query(() => 'yay!'),
+  secret: secretRouter,
 });
 
 export type AppRouter = typeof appRouter;
---
+++ src/server/routers/secret/secret.ts
@@ -0,0 +1,34 @@
+/**
+ *
+ * This is an example router, you can delete this file and then update `../pages/api/trpc/[trpc].tsx`
+ */
+import { router, publicProcedure } from '@/server/lib';
+import { TRPCError } from '@trpc/server';
+import { v4 as uuidv4 } from 'uuid';
+import { z } from 'zod';
+
+export const secretRouter = router({
+  create: publicProcedure
+    .input(
+      z.object({
+        token: z.string().min(1),
+        max_views: z.number().int().optional(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const dbKey = uuidv4();
+      const secret = await ctx.db
+        .insertInto('secret')
+        .values({
+          token: input.token,
+          key: dbKey,
+          maxViews: input.max_views ?? -1,
+          ownerId: ctx.userId,
+        })
+        .execute();
+
+      return {
+        key: dbKey,
+      };
+    }),
+});
