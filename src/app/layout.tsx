868d092074b9ef71f9c1b00c49fed3b9a07790b7
GashonHussein <gashon96@gmail.com>
2023-05-25 20:19:30 -0700

    fix: trpc client w/ ssr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,17 @@
 'use client';
 import { Inter } from 'next/font/google';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { trpc, trpcClient, queryClient } from '@/lib';
+import { trpc } from '@/lib';
 
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={inter.className}>
-      <body>
-        <trpc.Provider client={trpcClient} queryClient={queryClient}>
-          <QueryClientProvider client={queryClient}>
-            {children}
-          </QueryClientProvider>
-        </trpc.Provider>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
+
+export default trpc.withTRPC(RootLayout);
--- src/lib/trpc.ts
+++ src/lib/trpc.ts
@@ -1,18 +1,20 @@
-'use client';
-import { createTRPCReact } from '@trpc/react-query';
+import { createTRPCNext } from '@trpc/next';
 import type { AppRouter } from '@/server/routers';
 import { httpBatchLink } from '@trpc/client';
 
-export const trpc = createTRPCReact<AppRouter>();
-
-export const trpcClient = trpc.createClient({
-  links: [
-    httpBatchLink({
-      url: 'http://localhost:3000/api/trpc',
-      // You can pass any HTTP headers you wish here
-      async headers() {
-        return {};
-      },
-    }),
-  ],
+export const trpc = createTRPCNext<AppRouter>({
+  config() {
+    return {
+      links: [
+        httpBatchLink({
+          url: 'http://localhost:3000/api/trpc',
+          // You can pass any HTTP headers you wish here
+          async headers() {
+            return {};
+          },
+        }),
+      ],
+    };
+  },
+  ssr: true,
 });
