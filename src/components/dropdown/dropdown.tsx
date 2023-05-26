75b22f3644191cbcaf4bf4c35feb26a9d4dd871c
GashonHussein <gashon96@gmail.com>
2023-05-25 23:55:43 -0700

    refactor: add dropdown comp

--- src/components/dropdown/dropdown.tsx
+++ src/components/dropdown/dropdown.tsx
@@ -9,19 +9,19 @@ export const DropDown: FC<DropDownProps> = ({ PreviewComponent, children }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
-    <div>
+    <>
       <div
         onClick={() => setIsOpen(!isOpen)}
         className="cursor-pointer flex flex-row items-center gap-2"
       >
         {isOpen ? (
-          <AiOutlineArrowUp className="inline-block" />
+          <AiOutlineArrowUp className="inline-block opacity-50" />
         ) : (
           <AiOutlineArrowDown className="inline-block" />
         )}
         {PreviewComponent}
       </div>
-      {isOpen && <div>{children}</div>}
-    </div>
+      {isOpen && <>{children}</>}
+    </>
   );
 };
--- src/features/secret/components/record-dropdown.tsx
+++ src/features/secret/components/record-dropdown.tsx
@@ -2,6 +2,31 @@
 import { DropDown } from '@/components';
 import { trpc } from '@/lib';
 
+function DropDownRecord({ secret }: any) {
+  return (
+    <div key={secret.key} className="flex flex-row gap-4 w-full my-2">
+      <p className="w-1/4 border-r-2 border-white opacity-50">
+        {secret.createdAt}
+      </p>
+
+      <div className="w-3/4">
+        <p className="w-full">
+          Key: <strong>{secret.key}</strong>
+        </p>
+        <div className="w-full flex justify-between items-center">
+          <p className="flex flex-row items-center">
+            Views: {secret.views}
+            {secret.maxViews != -1 && `/ ${secret.maxViews}`}
+          </p>
+          <p className="opacity-75">
+            {!secret.deletedAt ? 'Active' : 'Deleted'}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export function RecordDropDown() {
   const secrets = trpc.secret.list.useQuery();
 
@@ -9,26 +34,11 @@ export function RecordDropDown() {
     <DropDown PreviewComponent={<h4>Records</h4>}>
       <div className="text-white">
         {secrets.data?.map((secret: any) => (
-          <div key={secret.key} className="flex flex-row gap-4 w-full my-2">
-            <p className="w-1/4 border-r-2 border-white opacity-50">
-              {secret.createdAt}
-            </p>
-
-            <div className="w-3/4">
-              <p className="w-full">
-                Key: <strong>{secret.key}</strong>
-              </p>
-              <div className="w-full flex justify-between items-center">
-                <p className="flex flex-row items-center">
-                  Views: {secret.views}
-                  {secret.maxViews != -1 && `/ ${secret.maxViews}`}
-                </p>
-                <p className="opacity-75">
-                  {!secret.deletedAt ? 'Active' : 'Deleted'}
-                </p>
-              </div>
+          <DropDown PreviewComponent={<DropDownRecord secret={secret} />}>
+            <div className="w-full flex justify-end">
+              <div className="w-3/4 ">Contnet</div>
             </div>
-          </div>
+          </DropDown>
         ))}
       </div>
     </DropDown>
